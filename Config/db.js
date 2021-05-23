004a0045776cfdc1650a9d6fdd097400da55f57c
Ishwar Datt Mishra <ishwardattmishra@qainfotech.com>
2021-05-23 16:09:12 +0530

    db string

--- Config/db.js
+++ Config/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const con = mysql.createConnection(
-    { host: 'localhost', user: 'root', password: 'root', database: 'my_calendar' }
+"mysql://f9feern0wghywenw:am2fn399ewsm99zl@tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/pg8mava7lsr7looe"
 );
 
 
